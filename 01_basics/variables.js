9dfeef98b200729d026f7e90899c3f8595098dc2
theanishajain <anishajainfirefox@gmail.com>
2024-01-25 18:12:01 +0000

    new file

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 3444; //it's better to lock some variables with const
+let accountEmail = "anishayahoo@gmail.com";
+var accountPassword = "fjgj";
+accountCity = "Pune"
+// accountId = 57876; not possible
+accountEmail = "anisha@gmail.com"
+accountPassword = "gjg";
+accountCity = "Agra";
+console.table([accountId, accountEmail, accountPassword, accountCity])
