c026ffbb4b2a357ecd50867d9eade7afa7e60329
theanishajain <anishajainfirefox@gmail.com>
2024-01-25 20:18:59 +0000

    new code

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//now our code will be treated as the newer version of js
+
+//alert(3+3) cannot work here because we are using nodejs not browser
+
+//null
+//number
+//string
+//boolean
+//symbol
+//undefined
+//bigint
+
+console.log(typeof null);//when we try to know the datatype of null we get object in return 
+
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let number = 33;
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+let value = 3;
+let negValue = -value;
+
+// ****OPERATION**
+console.log(2**2);//will return the answer in power
+let str1 = "1";
+let str2 = "2";
+let str3 = str1 + str2;
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 4 + 4);
+console.log(+true);//will print 1
